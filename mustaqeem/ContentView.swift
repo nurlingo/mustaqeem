abbefd3593c46e19165918811fdf37660aeac653
Nursultan Askarbekuly <nursultan@outlook.com>
2019-06-11 02:38:03 +0300

    implement exemplary Credit Card form

--- mustaqeem/ContentView.swift
+++ mustaqeem/ContentView.swift
@@ -9,8 +9,81 @@
 import SwiftUI
 
 struct ContentView : View {
+    
+    @State var firstName = ""
+    @State var lastName = ""
+    
+    @State var users = [String]()
+    
     var body: some View {
-        Text("Hello World")
+        
+        NavigationView {
+            VStack {
+                
+                VStack {
+                    
+                    VStack {
+                        
+                        Group {
+                            TextField($firstName, placeholder: Text("First Name")).padding(12)
+                        }.background(Color.white)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .shadow(radius: 5)
+                        
+                        Group {
+                            TextField($lastName, placeholder: Text("Last Name")).padding(12)
+                            }.background(Color.white)
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                            .shadow(radius: 5)
+                        
+                        
+                        
+                        HStack {
+                            
+                            Button(action: {
+                                
+                                self.users.append("\(self.firstName) \(self.lastName)")
+                                self.firstName = ""
+                                self.lastName = ""
+                                
+                            }) {
+                                Group {
+                                    Text("Create user").color(.white).padding(12)
+                                    }.background((firstName.count > 0 && lastName.count > 0 ? Color.blue : Color.gray))
+                                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                                    .shadow(radius: 5)
+                            }.disabled(firstName.count == 0 || lastName.count == 0)
+                            
+                            Button(action: {
+                                
+                                self.users = []
+                                
+                            }) {
+                                Group {
+                                    Text("Delete users").color(.white).padding(12)
+                                    }.background(Color.red)
+                                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                                    .shadow(radius: 5)
+                            }
+                            
+                        }
+                        
+                        
+                    }.padding(12)
+                    
+                }.background(Color.gray)
+                
+                List (users.identified(by: \.self)) {
+                    Text($0)
+                }
+            }.navigationBarTitle(Text("Credit Card Form"))
+                .navigationBarItems(leading: HStack {
+                    Text("First name:")
+                    Text(firstName).color(.red)
+                    Text("Last name:")
+                    Text(lastName).color(.red)
+                })
+        }
     }
 }
 
